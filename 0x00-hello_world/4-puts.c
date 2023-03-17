2ce059b6064a63309ee3e3869aa581c899adb8fc
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-16 17:13:24 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
-/** 
- * main - print to screen 
+/**
+ * main - print to screen
  *
  * Return:zero
- * /
- int main(void)
- {
-     char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-     puts(Mystr);
-
-     return(0);
-
-  }
-
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
