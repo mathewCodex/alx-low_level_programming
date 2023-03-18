b541abb64f3d6814a65a340640d706c5efb2d29e
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-17 18:26:48 -0700

    Printing lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <sdio.h>
+
+/**
+ * main - Print lowercase Alphabet
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
