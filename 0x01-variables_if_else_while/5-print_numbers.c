2c61a2a8f907e3937a9fca3c7c4c2b5a1296b3dd
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-17 19:28:13 -0700

    Printing numerics

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numeric
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf('\n');
+	return (0);
+}
