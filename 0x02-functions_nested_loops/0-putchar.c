57b192b05bcbc16402122c050b7e61b2da1b0218
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-21 09:20:53 -0700

    print putchar in a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+ * main - debugging example
+ * Return: 0
+ */
+int main(void)
+{
+	char *putchar = "_putchar";
+
+	while (*putchar)
+	{
+		_putchar(*putchar);
+		holberton++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# Fucntions and nested Loops
