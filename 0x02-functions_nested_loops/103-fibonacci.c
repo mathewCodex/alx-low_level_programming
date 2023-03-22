619dde3a4876cb3afd86db189b3123a0c913e9f3
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-21 17:57:29 -0700

    fibo 3

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * purpose - fib0nacci
+ *
+ * Return: (0) awlays
+ */
+
+int main(void)
+{
+	unsigned long int lim = 4000000;
+	unsigned long int bef = 0;
+	unsigned long int aft = 1;
+	unsigned long int R = 0;
+	unsigned long int sum = 0;
+		while (lim > R)
+		{
+			R = bef + aft;
+
+			if ((R % 2) == 0)
+			{
+				sum += R;
+			}
+			bef = aft;
+			aft = R;
+		}
+		printf("li\n", sum);
+		return (0);
+}
