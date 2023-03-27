801e0647c47f5ff35ef42bb31173689880f7af2a
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-27 09:21:53 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_strcpy - copies the string pointed to by src.
@@ -7,36 +8,12 @@
  * @src: pointer to src str.
  * Return: char
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int n;
-	int i;
+	char *f = dest;
 
-	n = string_length(src);
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\n';
-	}
-	return (dest);
-}
-
-
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
+	while (*src)
+		*dest++ = *src++;
+	return (f);
 }
