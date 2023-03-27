2814534012a4a6a4ee33afa0f49c3d5a4fb45635
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-27 09:15:38 -0700

    pointer to the str

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src.
+ *
+ * @dest: pointer to buff.
+ * @src: pointer to src str.
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+	int i;
+
+	n = string_length(src);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n i++)
+	{
+		dest[i] = '\n';
+	}
+	return (dest);
+}
+
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
