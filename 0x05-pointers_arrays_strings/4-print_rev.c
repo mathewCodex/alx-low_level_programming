bb79799dcf95e38024420ba22f03febe774b623f
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-27 08:13:45 -0700

    Na only way to heaven

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse
+ * @s: string input
+ * Return: Always 0 (success)
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	char *rev = s + length - 1;
+
+
+	while (rev >= s)
+	{
+		printf("%c", *rev);
+		rev--;
+	}
+	printf('\n');
+}
