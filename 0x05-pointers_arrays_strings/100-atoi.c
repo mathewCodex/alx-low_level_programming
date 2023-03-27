05251d14b2e749f553bcb0d8903a7a4ed080395b
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-27 09:50:59 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,32 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
-  * main - generates random passwords for 101-crackme
-  * Return: zero
-  */
-int main(void)
-{
-	int sum;
-	char c;
+ * _atoi - conert a string to an integer
+ * @s: input
+ * Return: Always 0.
+ */
 
-	srand(time(NULL));
-	while (sum <= 2645)
+int _atoi(char *s)
+{
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
+
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		if (s[i] == '-')
+		sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res = res - (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
 	}
-	putchar(2772 - sum);
-	return (0);
+	res = sig * res;
+	return (res);
 }
