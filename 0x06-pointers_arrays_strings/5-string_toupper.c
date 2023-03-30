c811e0b0a43cf81b1a9d9c3bea828545baa11b5a
mathewCodex <mathewemmanuel212@gmail.com>
2023-03-30 06:39:07 -0700

    I shall go from lower to UPPER in Jesus name Amen!

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase letters to upper
+ * @str: str to return.
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
+
