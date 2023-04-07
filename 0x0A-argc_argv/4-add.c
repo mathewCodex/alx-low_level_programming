665f4774d671af7f3ab83d196caba6a795cb7878
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-06 20:07:00 -0700

    print name of programs

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of program
+ * @argc:command line argument count
+ * @argv:command line arguement array
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, res, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	res = 0, j = 0;
+	for (i = 1; i < argc; i++)
+	{
+		while (argv[i][j])
+		{
+			if (!(argv[i][j] >= "1" && argv[i][j] <= "9"))
+			{
+				printf("Error\n");
+				return (0);
+			}
+			j++;
+		}
+		re += strtol(argv[i], NULL, 10);
+	}
+	printf("%d\n", res);
+	return (0);
+}
