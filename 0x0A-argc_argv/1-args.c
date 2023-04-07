0e98348a3f80404d3a6182aa1deb65adfe61d1c5
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-06 19:44:07 -0700

    cisfun

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints its name, followed by new.
+  * @argc: argument counter.
+  * @argv: argument vector.
+  * Return: zero.
+  */
+int main(int argc, char *argv[])
+{
+	printf("5d\n", argc - 1);
+
+	return (sizeof(argv[0]) - sizeof(argv[0]));
+}
