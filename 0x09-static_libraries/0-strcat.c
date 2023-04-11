27810baa09e3ce950959207447fc661559370e25
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-11 13:52:16 -0700

    creating file n objects

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,37 +1,24 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings.
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
- * @dest: resulting string.
- * @src: source string.
- * Return: pointer to dest
+ * Return: returns poiner to @dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, n;
 
-	n = string_length(dest);
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[n + i] = src[i];
-	}
-	dest[n + i] = '\0';
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
-}
-
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
 }
