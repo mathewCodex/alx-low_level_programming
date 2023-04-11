d7b818b448c2fde3d4a7d60a02f01a4bbce3117c
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-11 06:43:11 -0700

    static lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints the string folowed by a new line
+ * @str: Pointer to the input
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+# 0x09 my First static library
