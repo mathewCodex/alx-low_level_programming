e71b46195dfc7195f7ee39e1e1815be082d4b066
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-11 08:34:52 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,48 +11,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, n;
-	char *new_str;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	n = string_length(s1) + string_length(s2) + 1;
-	new_str = malloc(sizeof(char) * n);
-	/*if null passed */
-	if (new_str == NULL)
+
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
 		return (NULL);
-	/*insert s1 */
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		new_str[j] = s1[i];
+		conct[i] = s1[i];
 		i++;
-		j++;
 	}
-	/*insert s2*/
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		new_str[j] = s2[i];
-		i++;
-		j++;
-	}
-	new_str[j] = '\0';
-	return (new_str);
-}
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
 
-	while (*(pointer + c) != '\0')
+	while (s2[ci] != '\0')
 	{
-		c++;
+		conct[i] = s2[ci];
+		i++, ci++;
 	}
-	return (c);
+	conct[i] = '\0';
+	return (conct);
 }
+
