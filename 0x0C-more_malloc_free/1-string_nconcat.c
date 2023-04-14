ea7eed041c1ee6258b525743d702be245e036dce
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-14 01:43:59 -0700

    str concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,28 +13,35 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, i;
+	char  *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 == "";
-	for (i = 0; s1[i]; i++)
-		len++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	concat = malloc(sizeof(char) * (len + 1));
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (concat == NULL)
+	if (!s)
 		return (NULL);
 
-	len = 0;
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-	for (i = 0; s1[i]; i++)
-		concat[len++] = s1[i];
-	for (i = 0; s2[i] && i < n; i++)
-		concat[len++] = s2[i];
-	concat[len] = '\0';
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	return (concat);
+	while (n >= len2 && i < (len1 + len2))
+	s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
