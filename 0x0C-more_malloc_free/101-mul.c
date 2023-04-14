d7fafe239b373dc3fbf25cd5c154b2ee83d04585
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-14 03:00:59 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,96 +1,90 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * is_dig - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_dig(char *s)
-{
-	int i = 0;
+void print_error(void);
+int is_valid_number(char *number);
+void multiply(char *num1, char *num2);
+void reverse_string(char *str);
 
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-/**
- * errors - handles err for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-/**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
- */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2;
-	int len1, len2, len, i, carry, dig1, dig2, *result, a = 0;
+    if (argc != 3 || !is_valid_number(argv[1]) || !is_valid_number(argv[2])) {
+        print_error();
+    }
+    else {
+        multiply(argv[1], argv[2]);
+    }
 
-	num1 = argv[1], num2 = argv[2];
-	if (argc != 3 || !is_dig(s1) || !is_dig(s2))
-		errors();
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i < len1 + len2; i++)
-		result[i] = i0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		digit1 = num1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = num2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (dig1 * dig2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
-	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
-	return (0);
+    return 0;
 }
+
+void print_error(void)
+{
+    printf("Error\n");
+    exit(98);
+}
+int is_valid_number(char *number)
+{
+    for (int i = 0; number[i] != '\0'; i++) {
+        if (!isdigit(number[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void multiply(char *num1, char *num2)
+{
+    int len1 = strlen(num1);
+    int len2 = strlen(num2);
+    int result_len = len1 + len2;
+    int carry = 0;
+    int digit1, digit2, product;
+    int result[result_len];
+    memset(result, 0, result_len * sizeof(int));
+
+    // Perform multiplication
+    for (int i = len1 - 1; i >= 0; i--) {
+        carry = 0;
+        digit1 = num1[i] - '0';
+
+        for (int j = len2 - 1; j >= 0; j--) {
+            digit2 = num2[j] - '0';
+            product = digit1 * digit2 + carry + result[i + j + 1];
+            result[i + j + 1] = product % 10;
+            carry = product / 10;
+        }
+        result[i] += carry;
+    }
+
+    // Convert result array to string
+    char result_str[result_len + 1];
+    memset(result_str, '\0', result_len + 1);
+    for (int i = 0; i < result_len; i++) {
+        result_str[i] = result[i] + '0';
+    }
+
+    // Remove leading zeros
+    int i = 0;
+    while (result_str[i] == '0' && i < result_len - 1) {
+        i++;
+    }
+    reverse_string(result_str + i);
+
+    // Print result
+    printf("%s\n", result_str);
+}
+
+void reverse_string(char *str)
+{
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char tmp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = tmp;
+    }
+}
+
