f82afa97b7e798a50972f0e854aae661fde4b2e3
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-19 12:09:07 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbs
+ * @a: first numb
+ * @b: second numb
+ * Return: a + b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two num
+ * @a: first num
+ * @b: second num
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two num
+ * @a: first numb
+ * @b: second numb
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * * op_mod - finds modulus of two numb
+ * @a: first numb
+ * @b: second numb
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
