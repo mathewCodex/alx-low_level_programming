0d40ca910900ef255a932fe9ab75e117238eb3ff
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-20 05:17:42 -0700

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,19 +11,23 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list li;
 	unsigned int i;
-	va_list strings;
-	char *string;
+	char *str;
 
-	va_start(strings, n);
+	va_start(li, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strings, char *);
-		printf("%s", str != NULL ? str : "(nil)");
-		if (i < (n - 1) && separator != NULL)
+		str = va_arg(li, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
-	va_end(strings);
+	va_end(li);
 }
