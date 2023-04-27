d32cfa7f501661037597aadb3bc9f55ca866eac7
mathewCodex <mathewemmanuel212@gmail.com>
2023-04-27 08:41:29 -0700

    Add node at end

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,47 +1,40 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
-  * *add_node - adds a new node at the beginning of a list_t list.
-  * @head: pointer to first elements.
-  * @str: string.
-  * Return: the address of the new elements,
-  * or NULL if it failed.
-  */
+ * add_node - adds a new node at the beginning of a list
+ *@head: pointer to the head of the list
+ *@str: string to be added
+ *
+ * Return: returns the address to the new element or NULL
+ * if failed
+*/
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+	char *dup;
+	int len;
+	list_t *new;
 
-	if (head != NULL && str != NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
 	{
-		temp = malloc(sizeof(list_t));
-		if (temp == NULL)
-			return (NULL);
-
-		temp->str = strdup(str);
-		temp->len = _strlen(str);
-		temp->next = *head;
-
-		*head = temp;
-		return (temp);
+		free(new);
+		return (NULL);
 	}
+	for (len = 0; str[len];)
+		len++;
 
-	return (0);
-}
-/**
- * _strlen - ---find the length
- * Return: -Str length
- * @s: Str to count
- */
-int _strlen(const char *s)
-{
-	int c = 0;
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
 
-	while (*s)
-	{
-		s++;
-		c++;
-	}
-	return (c);
+	*head = new;
+
+	return (new);
+
 }
+
