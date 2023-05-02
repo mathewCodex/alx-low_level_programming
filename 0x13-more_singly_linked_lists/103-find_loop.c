7e055fda49322217e7a38c288bf91db588a4c452
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-02 15:42:22 -0700

    slow and fast

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - ******************
+ * @head: linked list tos saerch
+ * Return: address of node where loop starts
+ */
+listint *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+	return (NULL);
+}
