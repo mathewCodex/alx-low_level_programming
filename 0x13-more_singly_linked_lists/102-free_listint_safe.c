83e2b77f6c61721796065a7ed3107b5b807000e7
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-02 15:24:28 -0700

    free as a bird

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free a linked
+ * @h: pointer to the first node
+ * Return: numb of elems in the freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t initialLen = 0;
+	int difference;
+	listint_t *t;
+
+	if (!h || *h)
+		return (0);
+
+	while (*h)
+	{
+		difference = *h - (*h)->next;
+		if (difference > 0)
+		{
+			t = (*h)->next;
+			free(*h);
+			*h = t;
+			initialLen++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			initialLen++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (initialLen);
+}
