e63f547e8551942046afa2fedab28ecb113fab4e
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-02 06:47:54 -0700

    fnction that insert a new node at a given pos

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * *insert_nodeint_at_index - input a new node at a given pos
+ * @head: inde of the list where the new node should be added
+ * @idx: index of the list where node should be inputed
+ * @n: elem to input
+ * Return: address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *t, *newNode;
+	/* providing more space using malloc */
+	t = *head;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = NULL;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	/* looping thru i */
+	for (i = 0; i < idx - 1; i++)
+	{
+		t = t->next;
+		if (t == NULL)
+		{
+			return (NULL);
+		}
+	}
+	newNode->next = t->next;
+	t->next = newNode;
+	return (newNode);
+}
