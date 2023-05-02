8a8baa4ad8de7bd40ca7f9a7e0d47c2b26a17b58
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-02 06:13:52 -0700

    A code that returns the sum of all nth data in a link

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * sum_listint - returns sum of all the data(n) of a linked list
+ * @head: pointer
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int initialSumVal = 0;
+	listint_t *t;
+
+	if (head == NULL)
+		return (0);
+	t = head;
+	while (t != NULL)
+	{
+		sum += temp->n;
+		t = t->next;
+	}
+	return (initialSumVal);
+}
