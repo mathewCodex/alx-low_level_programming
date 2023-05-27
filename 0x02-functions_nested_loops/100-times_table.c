5b6137c54246e0afb3a9d86e26c7175d3d979ac6
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-27 10:58:04 -0700

    time table functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,50 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table
- * @n: integer for which the times table will be printed
- * Return: Always 0 (Success)
+ * print_times_table - prints  thew time table
+ * @n: int for which the times table will be printed
+ * Return: Zero all day
  */
-
 void print_times_table(int n)
-{
-	int num, mult, prod;
+	{
+		int i, j, k;
 
-if (n >= 0 && n <= 15)
-{
 
-for (num = 0; num <= n; num++)
-{
-_putchar('0');
-for (mult = 1; mult <= n; mult)
-{
+		if (n >= 0 && n <= 15)
+		{
+			for (i = 0; i <= n; i++)
+			{
+				for (j = 0; j <= n; j++)
+				{
+					k = j * i;
+					if (j == 0)
+					{
+						_putchar(k + '0');
+					} else if (k < 10 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					} else if (k >= 10 && k < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					} else if (k >= 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+				}
+				_putchar('\n');
+			}
+		}
+	}
 
-prod = num * mult;
-
-if (prod <= 99)
-	_putchar(' ');
-if (prod <= 9)
-	_putchar(' ');
-if (prod >= 100)
-{
-	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
-}
-else if (prod <= 99 && prod >= 10)
-{
-	_putchar((prod % 10) + '0');
-}
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
-}
-}
