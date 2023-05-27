f4ad8848b102c18edfc102f1ab5a0258b6531dad
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-27 13:03:26 -0700

    C mode

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - returns the value of a bit at a given idx
  * @n: number
  * @index: pos
- * Return: value of bit idex 
+ * Return: value of bit idex
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
