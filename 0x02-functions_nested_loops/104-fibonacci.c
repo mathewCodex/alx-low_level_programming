30f0ea25db5abf9b516fed62b2649b02d9d62824
mathewCodex <mathewemmanuel212@gmail.com>
2023-05-27 11:34:06 -0700

    fibonnaci correction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,45 +3,34 @@
 /**
  * main - fibonacci <3
  *
- * purpose - no hardcode
- *
- * Return: (success)
+ * purpose - no hard core
+ * Return: success
  */
-
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
-
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", aft);
-		bef = aft - bef;
-
-	}
-
-	bef1 = (bef / 1);
-	bef2 = (bef % 1);
-	aft1 = (aft / 1);
-	aft2 = (aft % 1);
-
-	for (i = 92; i < 99; ++i)
-	{
-	printf(", %lu", aft1 + (aft2 / 1));
-	printf("%lu", aft2 % 1);
-	aft1 = aft1 + aft1;
-	bef1 = aft1 - bef1;
-	aft2 = aft2 + bef2;
-	bef2 = aft2 - bef2;
-	}
-	printf("\n");
-	return (0);
+unsigned long int i, j, k, j1, j2, k1, k2;
+j = 1;
+k = 2;
+printf("%lu", j);
+for (i = 1; i < 91; i++)
+{
+printf(", %lu", k);
+k = k + j;
+j = k - j;
+}
+j1 = j / 1000000000;
+j2 = j % 1000000000;
+k1 = k / 1000000000;
+k2 = k % 1000000000;
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", k1 + (k2 / 1000000000));
+printf("%lu", k2 % 1000000000);
+k1 = k1 + j1;
+j1 = k1 - j1;
+k2 = k2 + j2;
+j2 = k2 - j2;
+}
+printf("\n");
+return (0);
 }
